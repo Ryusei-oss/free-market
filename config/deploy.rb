4588b06a969953fedb440af9dba20430cd7ef9b3
Ryusei-oss <ryusei945@gmail.com>
2020-07-19 16:33:13 +0900

    basic認証20.07.19

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.1"
 
 set :application, "free-market"
-set :repo_url, "git@example.com:Ryusei-oss/free-market.git"
+set :repo_url, "git@github.com:Ryusei-oss/free-market.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
