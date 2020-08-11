75f6ce4d5728e8084912297099f581efaf7c8510
Ryusei-oss <ryusei945@gmail.com>
2020-08-11 21:51:01 +0900

    Edit linker

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'items#index'
+  root 'items#mypage'
+  get '/items/logout', to: 'items#logout'
+  get '/items/register', to: 'items#register'
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
