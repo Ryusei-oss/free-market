0fe5c6a1fa6a877a6a8b2378f8f6a36ea3f233c6
Ryusei-oss <ryusei945@gmail.com>
2020-08-15 11:16:06 +0900

    ルーティングの整理、prefixの使用

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  root 'items#mypage'
-  get '/items/logout', to: 'items#logout'
-  get '/items/register', to: 'items#register'
   resources :items
-  root 'items#index'
+  get '/logout', to: 'items#logout'
+  get '/register', to: 'items#register'
+  root 'items#mypage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
