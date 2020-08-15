045fde65338d7ce9532173d6c1b74e84f279b1b4
Ryusei-oss <ryusei945@gmail.com>
2020-08-15 11:36:14 +0900

    商品詳細ページへのリンク設定＋出品ページの画像表示の修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :items
   get '/logout', to: 'items#logout'
   get '/register', to: 'items#register'
+  get '/product', to: 'items#product'
   root 'items#mypage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
