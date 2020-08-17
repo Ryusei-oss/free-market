de5f4ee52f5710f3e3e4bd930e25d6d31f6ebcac
Okahiro3 <okazaki1995@icloud.com>
2020-08-17 11:54:25 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  resources :items
-  get '/logout', to: 'items#logout'
-  get '/register', to: 'items#register'
-  get '/product', to: 'items#product'
-  root 'items#mypage'
+
+  root 'items#purchase'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
