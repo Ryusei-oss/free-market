0e3851380f86aa1ff4cfff689c8504da320a84e7
Okahiro3 <okazaki1995@icloud.com>
2020-08-17 18:56:35 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root 'items#sessions'
+  resources :items
+  get '/registrations', to: 'items#registrations'
+  get '/sessions', to: 'items#sessions'
+  root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
