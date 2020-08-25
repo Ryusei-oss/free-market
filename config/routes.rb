2d28c6c42b58f2760770f09b98b0876b91fcbdb2
Okahiro3 <okazaki1995@icloud.com>
2020-08-25 12:22:54 +0900

    New 商品購入確認ページ

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/logout', to: 'items#logout'
   get '/register', to: 'items#register'
   get '/product', to: 'items#product'
-  root 'items#mypage'
+  root 'items#purchase'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
