c51a84fb7718a17cedfcbed88b1b14b4ae8784f8
Ryusei-oss <ryusei945@gmail.com>
2020-08-29 10:58:02 +0900

    アドレステストコードの雛形実装
    上記

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :address do
-    
+    association :user
+    postal_code {"12345"}
+    area        {"大阪府"}
+    city        {"大阪市"}
+    house_number{"1234"}
   end
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe Address, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+#   let(:user) { FactoryBot.create(:user) }
+#   let(:address) { FactoryBot.create(:address, user_id: user.id) }
+# end
+describe Address do
+  describe '#create' do
+    it "" do
+      address = build(:address)
+      expect(address).to be_valid
+    end
+
+    it "Check postal_code Nil" do
+      address = build(:address, postal_code: nil)
+      address.valid?
+      expect(address.errors[:postal_code]).to include("can't be blank")
+    end
+  end
 end
