f41a539388b6d59841f2cdf3d587d1656f0b9509
hasuke0324 <65121426+hasuke0324@users.noreply.github.com>
2020-08-30 00:29:13 +0900

    update0829h

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -8,3 +8,27 @@ $(function(){
 
     
   });
+
+// 商品説明文カウント
+$(function(){
+  $('.main__description-box').keyup(function(){
+    var count = $(this).val().length;
+    $('.main__description__counter-js').text(count);
+  });
+});
+
+//販売価格手数料利益計算
+$(function(){
+  $('.main__price__selling__price__box-input').change(function(){
+    var inputPrice = Number($(this).val());
+    if(inputPrice >= 300 && inputPrice < 10000000){
+      var fee = Math.floor(inputPrice * 0.1);
+      var profit = inputPrice - fee;
+      $('.fee_value').text("¥" + fee.toLocaleString()),
+      $('.profit_value').text("¥" + profit.toLocaleString())
+    }else{
+      $('.fee_value').text("ー");
+      $('.profit_value').text("ー");
+    }
+  });
+});
