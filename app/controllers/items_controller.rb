4a96c83b0f2019c106c8b9d4214e343438a3fdcb
Ryusei-oss <ryusei945@gmail.com>
2020-09-08 01:56:02 +0900

    カテゴリ充実テンプレート作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   def index
-    @category = Category.all.order("id ASC").limit(4)
+    #データベースへの反映がうまくいったらcategoriesテーブルのancestryカラムがnullになっている
+    #一番最大のidをlimit()内に書く
+    @category = Category.all.order("id ASC").limit(1)
   end
 
 end
