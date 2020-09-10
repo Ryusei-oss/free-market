fa2a9a5d4d4b517cd4af1ba5facd45d43b379c57
Ryusei-oss <ryusei945@gmail.com>
2020-09-11 02:09:10 +0900

    プルダウンメニューのアルゴリズム完成
    プルダウンメニューのアルゴリズムを実装した
    データを充実させれば完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   def index
     #データベースへの反映がうまくいったらcategoriesテーブルのancestryカラムがnullになっている
     #一番最大のidをlimit()内に書く
-    @category = Category.all.order("id ASC").limit(1)
+    @category = Category.all.order("id ASC").limit(3)
   end
 
 end
