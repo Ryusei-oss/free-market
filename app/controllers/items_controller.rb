5c5c000e050ef22852b5a528ec17900afbd3c4ba
yutatommy <blackarist0407@gmail.com>
2020-09-11 20:36:11 +0900

    commit1 名前・写真表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,12 @@
 class ItemsController < ApplicationController
+
   def index
-    
+    @products = Product.includes([:images, :user]).last(3).reverse
+    @images = Image.all
+  end
+
+  def items
+    @products = Product.includes([:images, :user]).order('created_at desc')
   end
 
 
