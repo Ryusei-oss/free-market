767409022be8a25d19d4195fae6a74ed24de91a5
yutatommy <blackarist0407@gmail.com>
2020-09-15 22:20:29 +0900

    commit1 ブランチを切るため

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -31,10 +31,13 @@ class ProductsController < ApplicationController
     end
   
     def edit
+      
     end
 
     def update
     end
+
+
 
     def destroy
       @product = Product.find(params[:id])
