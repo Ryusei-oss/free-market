61189ff880e8af029850d9daf50b171167ee2c3d
hasuke0324 <65121426+hasuke0324@users.noreply.github.com>
2020-09-24 23:24:35 +0900

    Update products_controller_spec.rb
    newとcreateコントローラーは通っている

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 describe ProductsController do
   let(:category) { create(:category) }
+  let(:user) { create(:user) }
 
   describe 'GET #new' do
     it "new.html.hamlに遷移すること" do
+      sign_in(user)
       get :new
       expect(response).to render_template :new
     end
@@ -12,41 +14,48 @@ describe ProductsController do
 
   describe 'POST #create' do
     it "商品が正しく登録されること" do
+      sign_in(user)
       product_params = attributes_for(:product, category_id: category.id)
       expect{ post :create, params: { product: product_params } }.to change(Product, :count).by(1)
     end
 
     it "登録成功時に root_pathに遷移すること" do
+      sign_in(user)
       product_params = attributes_for(:product, category_id: category.id)
       post :create, params: { product: product_params }
       expect(response).to redirect_to(root_path)
     end
 
     it "登録失敗時に new.html.hamlに遷移すること" do
+      sign_in(user)
       product_params = attributes_for(:product)
       post :create, params: { product: product_params }
       expect(response).to redirect_to new_product_path
     end
   end
 
-  # 編集アクション実装中
-  # describe 'GET #edit' do
-  #   it "商品が正しく編集されること" do
-  #     product_params = attributes_for(:product, category_id: category.id)
-  #     get :edit, params: { product: product_params }
-  #     expect(response).to have_http_status "200"
-  #   end
+  describe 'GET #edit' do
+    it "編集画面に正しく遷移すること" do
+      sign_in(user)
+      product_params = attributes_for(:product, category_id: category.id)
+      get :edit, params: { product: product_params }
+      expect(response).to render_template :edit
+    end
+  end
 
-  #   it "編集成功時に商品詳細画面 product_pathに遷移すること" do
-  #     product_params = attributes_for(:product, category_id: category.id)
-  #     get :edit, params: { product: product_params }
-  #     expect(response).to redirect_to(product_path)
-  #   end
+  describe 'PATCH #update' do
+    it "編集成功時に商品詳細画面 product_pathに遷移すること" do
+      sign_in(user)
+      product_params = attributes_for(:product, category_id: category.id)
+      patch :update, params: { product: product_params }
+      expect(response).to render_template :show
+    end
 
-  #   it "編集失敗時に編集画面のまま留まっているか"
-  #     product_params = attributes_for(:product)
-  #     get :edit, params: { product: product_params }
-  #     expect(response).to redirect_to(edit_product_path)
-  #   end
-  # end
+    it "編集失敗時に編集画面のまま留まっているか" do
+      sign_in(user)
+      product_params = attributes_for(:product)
+      patch :update, params: { product: product_params }
+      expect(response).to render_template :edit
+    end
+  end
 end
