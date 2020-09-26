4e268450d1710c6e8e1973c5d03a8f3364b66e78
Ryusei-oss <ryusei945@gmail.com>
2020-09-26 12:08:45 +0900

    購入機能の動作確認＆修正
    顧客情報とカード情報を紐付けた
    登録したクレジットカードで支払いができるようにした
    クレジットカードがないと支払いはできない（indexへリダイレクト)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,7 +12,13 @@ class ItemsController < ApplicationController
   end
 
   def purchase
+    card = Card.where(user_id: current_user.id)
+    binding.pry
+    if !card.present?
+      redirect_to items_path
+    end
     @product = Product.find(params[:product_id])
+    
   end
 
   def create
@@ -24,18 +30,16 @@ class ItemsController < ApplicationController
     @purchase = Purchase.new(user_id: current_user.id, product_id: params[:product_id])
     @purchase.save
     Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_SECRET_KEY]
-    @cards = Card.where(user_id: current_user.id)
-    customer = Payjp::Customer.retrieve(@cards.customer_id)
-    card = customer.cards.retrieve(@cards.card_id)
+    @card = Card.find_by(user_id: current_user.id)
     begin
       Payjp::Charge.create(
         amount: @product.price, # 決済する値段
-        card: card, # フォームを送信すると作成・送信されてくるトークン
+        customer: @card.customer_id, # フォームを送信すると作成・送信されてくるトークン
         currency: 'jpy')
+      @product.trading_status = '売り切れ'
+      @product.save
     rescue Payjp::InvalidRequestError
       redirect_to items_path
     end
-    @product.trading_status = '売り切れ'
-    @product.save
   end
 end
