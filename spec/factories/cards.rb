c1f63cccafe107f0fb4e6c00805be46fa2d120d1
Ryusei-oss <ryusei945@gmail.com>
2020-09-29 22:45:33 +0900

    単体テストの実装　商品購入機能
    完了

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :card do
     association :user
+    customer_id  {"cus_be7d25d0fe18b2e5994f59768c55"}
+    card_id {"car_644c44cf6be4faf6486dca031c07"}
   end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,11 +2,36 @@ require 'rails_helper'
 
 RSpec.describe Card, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
-  #=============================================================
-  # OK時のテスト
-  #=============================================================
+  describe "create" do
+    before do
+      @user = create(:user)
+    end
+    #=============================================================
+    # OK時のテスト
+    #=============================================================
+    it "When all param corrects" do
+      card = build(:card, user: @user)
+      expect(card).to be_valid
+    end
+    #=============================================================
+    # Nilデータ入っている時のテスト
+    #=============================================================
+    it "when card_id eq nil" do
+      card = build(:card, user: @user, card_id: nil)
+      card.valid?
+      expect(card.errors[:card_id]).to include("can't be blank")
+    end
 
-  #=============================================================
-  # Nilデータ入っている時のテスト
-  #=============================================================
+    it "when customer_id eq nil" do
+      card = build(:card, user: @user, customer_id: nil)
+      card.valid?
+      expect(card.errors[:customer_id]).to include("can't be blank")
+    end
+
+    it "when user_id eq nil" do
+      card = build(:card)
+      card.valid?
+      expect(card.errors[:user_id]).to include("can't be blank")
+    end
+  end
 end
